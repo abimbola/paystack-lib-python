630c9ba6833ec42b149352bbbec8567aec2d557b
Abimbola Adegun <abimbola.adegun@gmail.com>
2019-08-04 20:04:37 +0100

    corrected update method logic and add new module

--- paystacklib/plan.py
+++ paystacklib/plan.py
@@ -3,8 +3,8 @@ import paystacklib
 from paystacklib.baseapi import BaseApi
 from paystacklib.utils import clean_params
 
-class SubAccount(BaseApi):
-    object_type = '/subaccount'
+class Plan(BaseApi):
+    object_type = '/plan'
     def __init__(
             self, secret_key=None,
             uri=paystacklib.api_base + object_type, method=None, 
@@ -13,10 +13,8 @@ class SubAccount(BaseApi):
 
     @classmethod
     def create(
-            cls, business_name, settlement_bank, account_number, 
-            percentage_charge, primary_contact_email=None, 
-            primary_contact_name=None, primary_contact_phone=None, 
-            metadata=None, settlement_schedule=None): 
+            cls, name, amount, interval, description=None, send_invoices=None,
+            send_sms=None, currency=None, invoice_limit=None):
         params = locals()
         params = clean_params(params)
         uri = paystacklib.api_base + cls.object_type
@@ -25,28 +23,26 @@ class SubAccount(BaseApi):
 
     @classmethod
     def list(
-            cls, per_page=50, page=1): 
+            cls, per_page=50, page=1, interval=None, amount=None): 
         params = locals()
         params = clean_params(params)
         uri = paystacklib.api_base + cls.object_type 
         return cls(uri=uri, method='get', params=params).execute()
 
     @classmethod
-    def fetch(cls, id_or_slug):
+    def fetch(cls, id_or_plan_code):
         uri = paystacklib.api_base + \
-            '{0}/{1}'.format(cls.object_type, str(id_or_slug))
+            '{0}/{1}'.format(cls.object_type, str(id_or_plan_code))
         return cls(uri=uri, method='get').execute() 
 
     @classmethod    
     def update(
-            cls, id_or_slug, business_name=None, settlement_bank=None, 
-            account_number=None, percentage_charge=None,
-            primary_contact_email=None, description=None, 
-            primary_contact_name=None, primary_contact_phone=None,
-            metadata=None, settlement_schedule=None): 
+            cls, id_or_plan_code, name=None, 
+            amount=None, description=None, send_invoices=None,
+            send_sms=None, currency=None, invoice_limit=None): 
         params = locals()
         params = clean_params(params)
         uri = paystacklib.api_base + \
-            '{0}/{1}'.format(cls.object_type, str(id_or_slug)) 
+            '{0}/{1}'.format(cls.object_type, str(id_or_plan_code)) 
         return cls(uri=uri, method='put', params=params).execute()
 
