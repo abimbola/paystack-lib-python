b273b7795f65490540c38767a1dc99c7f01a92c3
Abimbola Adegun <abimbola.adegun@gmail.com>
2019-08-04 22:44:41 +0100

    Minor update

--- tests/test.py
+++ tests/test.py
@@ -12,13 +12,15 @@ class TransactionTests(unittest.TestCase):
         tinit = Transaction.initialize(50000, "0@test.com")
         self.assertEqual(tinit['status'], True, 'Transaction initialization failed')
 
-    def test_transaction_list_verify(self):
+    def test_transaction_list_verify_fetch(self):
         tlist = Transaction.list()
         self.assertEqual(tlist['status'], True, 'Transaction list failed') 
         tref = tlist['data'][0]['reference']
+        tid = tlist['data'][0]['id']
         tverify = Transaction.verify(tref)
         self.assertEqual(tverify['status'], True, 'Transaction verify failed')
-
+        tfetch = Transaction.fetch(tid)
+        self.assertEqual(tfetch['status'], True, 'Transaction fetch failed')
 if __name__ == '__main__':
     unittest.main()
 
